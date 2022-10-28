9d5e5525683398dc326a96d7feeb5864c898380e
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2022-10-28 23:25:24 +0100

    testing why the error boun dary is not working in production

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,24 +1,59 @@
-import React from 'react'
-import { Container} from "@mui/material"
-import Header from './ui_components/Header'
+import React, { useState } from "react";
+import { Container, Typography } from "@mui/material";
+import Header from "./ui_components/Header";
+import { ErrorBoundary } from "react-error-boundary";
 
-function Layout(props) {
+
+export const ExplodeContext = React.createContext([]) 
+
+function ErrorFallback({ error, resetErrorBoundary }) {
   return (
-    <Container disableGutters={true}>
-      <Header />
-      <Container
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: "70px",
-        }}
-      >
-        {props.children}
-      </Container>
-      <div className="copy">Copyright Oliyide Ibrahim @ AltSchools Africa </div>
+    <Container sx={{marginTop:"80px", textAlign:"center"}}>
+      <div role="alert">
+        <Typography component="div" variant="h4">
+          Something went wrong
+        </Typography>
+        <pre>{error.message}</pre>
+        <button
+          style={error ? { backgroundColor: "green" } : null}
+          className="explode"
+          onClick={resetErrorBoundary}
+        >
+          Reset
+        </button>
+      </div>
     </Container>
   );
 }
 
-export default Layout
+function Layout(props) {
+  const [explode, setExplode] = useState(false);
+  return (
+    <ExplodeContext.Provider value={[explode,setExplode]}>
+      <Container disableGutters={true}>
+        <Header />
+        <ErrorBoundary
+          FallbackComponent={ErrorFallback}
+          onReset={() => setExplode(false)}
+          resetKeys={[explode]}
+        >
+          <Container
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: "70px",
+            }}
+          >
+            {props.children}
+          </Container>
+        </ErrorBoundary>
+        <div className="copy">
+          Copyright Oliyide Ibrahim @ AltSchools Africa{" "}
+        </div>
+      </Container>
+    </ExplodeContext.Provider>
+  );
+}
+
+export default Layout;
