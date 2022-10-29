bcb87fa3429af1e6634c253e3912675611cef73b
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2022-10-29 02:45:58 +0100

    remove the import statement for css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 import Router from './components/routes/router';
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+
 
 
 
