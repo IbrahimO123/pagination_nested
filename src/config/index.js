e128a434c5bb79b970a1707a74291bf8daf79351
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2022-10-29 23:52:20 +0100

    used to store the firebase config

---
+++ src/config/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAS34-ah1DjvCfnMPRjHhcHcK4m8KK-QOg",
+  authDomain: "react-pagination-3e0ef.firebaseapp.com",
+  projectId: "react-pagination-3e0ef",
+  storageBucket: "react-pagination-3e0ef.appspot.com",
+  messagingSenderId: "396218337262",
+  appId: "1:396218337262:web:9674f8d625fc77689470ff",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
